c3df83c89b8ce572c87cf1b50de6c0357c1e9707
Latika Rai <lr5370566@gmail.com>
2024-05-31 18:25:35 +0530

    msg

---
+++ docs/script.js
@@ -0,0 +1,13 @@
+var tl = gsap.timeline();
+tl.to(".slide", {
+    scrollTrigger: {
+        trigger: ".bestseller",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1,
+        pin: true
+    },
+    xPercent: -430,
+    ease:Power4
+
+});
