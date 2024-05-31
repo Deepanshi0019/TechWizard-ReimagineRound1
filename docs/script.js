4437e75f319b95972f4c3e12e41efbae5339a006
Rahullchaudharyy <user.rahullchaudharryy@gmail.com>
2024-05-31 16:39:58 +0530

    first commit

---
+++ docs/script.js
@@ -0,0 +1,33 @@
+
+// Smooth background color transition
+const sections = document.querySelectorAll("main > div");
+const sectionColors = {
+    "Category-section": "#222222",
+    "The-press": "#000000",
+    "third-section": "#fefcbf"
+};
+
+sections.forEach(section => {
+    ScrollTrigger.create({
+        trigger: section,
+        start: "top 50%",
+        end: "bottom 50%",
+        onEnter: () => gsap.to("body", { backgroundColor: sectionColors[section.id], duration: 1 }),
+        onEnterBack: () => gsap.to("body", { backgroundColor: sectionColors[section.id], duration: 1 })
+    });
+});
+
+// Horizontal scrolling animation
+gsap.to("#News-Div", {
+    x: () => -(document.querySelector("#News-Div").scrollWidth - document.documentElement.clientWidth) + "px",
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#The-press",
+        start: "top top",
+        end: () => "+=" + document.querySelector("#News-Div").scrollWidth,
+        scrub: true,
+        pin: "#The-press",
+        anticipatePin: 1,
+    }
+});
+
