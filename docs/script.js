f31a78356703e6c2ecc85ff95e97f1d9263b254b
Latika <lr5370566@gmail.com>
2024-06-04 12:10:51 +0530

    msg

--- docs/script.js
+++ docs/script.js
@@ -1,13 +1,94 @@
-var tl = gsap.timeline();
-tl.to(".slide", {
-    scrollTrigger: {
-        trigger: ".bestseller",
-        start: "top top",
-        end: "bottom bottom",
-        scrub: 1,
-        pin: true
-    },
-    xPercent: -430,
-    ease:Power4
-});
+//bestseller
+var tl = gsap.timeline({scrollTrigger:{
+    trigger:".bestsellers",
+    markers:true,
+    start:"38% 50%",
+    end:"100% 50%",
+    scrub:2,
+    pin:true
+}});
+tl
+.to(".text",{
+    top: "-30%",
+},'a')
+.to("#card-one",{
+    top: "35%",
+},'a')
+.to("#card-two",{
+    top: "100%"
+},'a')
+.to("#card-two",{
+    top: "45%"
+},'b')
+.to("#card-one",{
+    width: "65%",
+    height: "65vh"
+},'b')
+.to("#card-three",{
+    top: "100%"
+}, 'b')
+.to("#card-three",{
+    top: "50%"
+}, 'c')
+.to("#card-two",{
+    width: "70%",
+    height: "70vh"
+},'c')
+.to("#card-four",{
+    top: "100%"
+},'c')
+.to("#card-four",{
+    top: "57%"
+},'d')
+.to("#card-three",{
+    width: "75%",
+    height: "75vh"
+},'d')
 
+//cursor
+// var bestsellers = document.querySelector(".bestsellers")
+// var cards = document.querySelector(".cards")
+// var cursor = document.querySelector(".cursor")
+
+// cards.addEventListener("mousemove", function(dets){
+//     gsap.to(cursor,{
+//         x:dets.x,
+//         y:dets.y
+//     })
+// })
+// cards.addEventListener("mouseenter",function(){
+//     gsap.to(cursor,{
+//         scale:1,
+//         opacity:0.8
+//     })
+// })
+// cards.addEventListener("mouseleave",function(){
+//     gsap.to(cursor,{
+//         scale:0,
+//         opacity:0
+//     })
+// })
+var bestsellers = document.querySelector(".bestsellers");
+var cards = document.querySelectorAll(".cards");
+var cursor = document.querySelector(".cursor");
+
+cards.forEach(function(card) {
+    card.addEventListener("mousemove", function(dets){
+        gsap.to(cursor, {
+            x: dets.clientX,
+            y: dets.clientY
+        });
+    });
+    card.addEventListener("mouseenter", function(){
+        gsap.to(cursor, {
+            scale: 1,
+            opacity: 0.8
+        });
+    });
+    card.addEventListener("mouseleave", function(){
+        gsap.to(cursor, {
+            scale: 0,
+            opacity: 0
+        });
+    });
+});
