661fd16005ca40242b4ad5107870ffead49b0506
Rahullchaudharyy <user.rahullchaudharryy@gmail.com>
2024-06-09 02:19:32 +0530

    Responsive in progress/Nav-bar is responsive now

--- docs/script.js
+++ docs/script.js
@@ -146,3 +146,20 @@ LifeStyleAnime();
 
 
 //                                    LifeStyle Ends Here  
+
+
+//                               For responsive
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const burgerIcon = document.querySelector('.burger-icon');
+    const openedMenu = document.getElementById('Opened-menu');
+
+    burgerIcon.addEventListener('click', () => {
+        if (openedMenu.classList.contains('hidden')) {
+            openedMenu.classList.remove('hidden');
+        } else {
+            openedMenu.classList.add('hidden');
+        }
+    });
+});
